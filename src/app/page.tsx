245fa349d380d0256ad9be6787f6f7d80a2b174f
NBX7777 <45907487+NBX7777@users.noreply.github.com>
2025-07-17 20:01:34 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '20vh', fontSize: '2rem' }}>
-      歡迎來到這個 Next.js 頁面！
+      歡迎來到這個 Hayden 的頁面！
     </div>
   );
 }
